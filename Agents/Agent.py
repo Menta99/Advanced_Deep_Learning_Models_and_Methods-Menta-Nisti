e9956c584e4f68429dc01c3862d7b9a9eb7da697
Menta99 <andrea.menta@mail.polimi.it>
2022-04-26 12:52:43 +0200

    Added NetworkBuilder class and Agent class template

---
+++ Agents/Agent.py
@@ -0,0 +1,25 @@
+class Agent:
+    def __init__(self, environment, batch_size, checkpoint_dir='tmp/agent_name', seed=42):
+        self.environment = environment
+        self.state_space_shape = self.environment.observation_space.shape
+        self.action_space_shape = self.environment.action_space.shape
+        self.batch_size = batch_size
+        self.learn_step_counter = 0
+        self.time_step = 0
+        self.checkpoint_dir = checkpoint_dir
+        self.seed = seed
+
+    def act(self, observation):
+        raise NotImplementedError("act method needs to be implemented by subclasses")
+
+    def store(self, transition):
+        raise NotImplementedError("store method needs to be implemented by subclasses")
+
+    def learn(self):
+        raise NotImplementedError("learn method needs to be implemented by subclasses")
+
+    def save(self):
+        raise NotImplementedError("save method needs to be implemented by subclasses")
+
+    def load(self):
+        raise NotImplementedError("load method needs to be implemented by subclasses")
---
+++ Utilities/NetworkBuilder.py
@@ -0,0 +1,56 @@
+import tensorflow as tf
+import traceback
+
+
+class NetworkBuilder:
+    def __init__(self):
+        self.supported_layers = {'Input': tf.keras.layers.Input,
+                                 'Dense': tf.keras.layers.Dense,
+                                 'Conv2D': tf.keras.layers.Conv2D,
+                                 'Activation': tf.keras.layers.Activation,
+                                 'BatchNormalization': tf.keras.layers.BatchNormalization,
+                                 'Dropout': tf.keras.layers.Dropout,
+                                 'Flatten': tf.keras.layers.Flatten,
+                                 'Concatenate': tf.keras.layers.Concatenate,
+                                 'GlobalAveragePooling2D': tf.keras.layers.GlobalAveragePooling2D,
+                                 'GlobalMaxPooling2D': tf.keras.layers.GlobalMaxPooling2D}
+
+    def build_network(self, params_dict):
+        """
+        Use the following format:
+        dict =
+            {0:
+                {'name': 'LAYER_NAME_0',
+                'params':
+                    {'PARAM_NAME_0_0': PARAM_VALUE_0_0,
+                    ...
+                    'PARAM_NAME_0_N':   PARAM_VALUE_0_N}
+                    }
+            ...
+            {N:
+                {'name': 'LAYER_NAME_N',
+                'params':
+                    {'PARAM_NAME_N_0': PARAM_VALUE_N_0,
+                    ...
+                    'PARAM_NAME_N_N': PARAM_VALUE_N_N}
+                    }
+                }
+            }
+        :param params_dict: dictionary of network layers information
+        :return: the sorted list of layers based on the params_dict
+        """
+        return [self.build_layer(layer_dict) for layer_dict in params_dict.values()]
+
+    def build_layer(self, layer_dict):
+        """
+        :param layer_dict: dictionary of layer information
+        :return: layer build with passed parameters
+        """
+        try:
+            return self.supported_layers[layer_dict['name']](**layer_dict['params'])
+        except KeyError:
+            print(traceback.format_exc())
+            print("Use one of the supported layers {}".format(list(self.supported_layers.keys())))
+        except TypeError:
+            print(traceback.format_exc())
+            print("Use valid parameters")
